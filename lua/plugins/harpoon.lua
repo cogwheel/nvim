c71ee5318219aa6de8e8ec16986fd8168e708d66
Matthew Orlando <maorlando@gmail.com>
2025-01-27 11:46:47 -0800

    Reformat

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,20 +1,20 @@
 local harpoon = function() return require("harpoon") end
 
 local function toggle_telescope(harpoon_files)
-    local conf = require("telescope.config").values
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
+  local conf = require("telescope.config").values
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
 
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
+  require("telescope.pickers").new({}, {
+    prompt_title = "Harpoon",
+    finder = require("telescope.finders").new_table({
+      results = file_paths,
+    }),
+    previewer = conf.file_previewer({}),
+    sorter = conf.generic_sorter({}),
+  }):find()
 end
 
 return {
