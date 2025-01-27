e58a6478349534343bb71d7c374dbef4e8ea5941
Matthew Orlando <maorlando@gmail.com>
2025-01-27 08:54:04 -0800

    Add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+	{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup{
+        ensure_installed = {
+          "c", "cpp", "lua", "vim", "vimdoc", "javascript", "html", "markdown", "python",
+        },
+        sync_install = false,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+      }
+    end,
+	},
+
+  {"nvim-treesitter/playground"},
+}
