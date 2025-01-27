817111ac67ee433bd29e2b8f70c85cf321e55548
Matthew Orlando <maorlando@gmail.com>
2025-01-27 11:15:48 -0800

    Add mason lsp support

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  'williamboman/mason.nvim',
+  dependencies = {
+    'williamboman/mason-lspconfig.nvim'
+  },
+  config = function()
+    require('mason').setup{}
+    require('mason-lspconfig').setup{
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup({})
+        end,
+      },
+    }
+  end,
+}
