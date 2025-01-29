ed68e4df883f8a1130de9e76d2695a8679089af1
Matthew Orlando <maorlando@gmail.com>
2025-01-28 22:47:28 -0800

    Transparent backgrounds

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,9 +1,33 @@
+function SetTransparency()
+  if vim.g.neovide then
+    -- use global transparency for gui
+    --vim.g.neovide_transparency = 0.95
+    vim.g.neovide_normal_opacity = 0.95
+  else
+    -- use terminal background color
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    --vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+    --vim.api.nvim_set_hl(0, 'NormalFloatNC', { bg = 'none' })
+
+    vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'none' })
+    --vim.api.nvim_set_hl(0, 'NeoTreeFloatNormal', { bg = 'none' })
+  end
+end
+
+-- Use transparent background color
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = '*',
+  callback = SetTransparency,
+})
+
 return {
-  "folke/tokyonight.nvim",
+  'folke/tokyonight.nvim',
   lazy = false,
   priority = 1000,
   opts = {},
   config = function()
-    vim.cmd.colorscheme("tokyonight-night")
+    vim.cmd.colorscheme('tokyonight-night')
   end,
 }
