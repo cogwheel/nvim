514d60b2896c2b951cd1d16abcb570b6cee99f3b
Matthew Orlando <maorlando@gmail.com>
2025-02-13 09:04:16 -0800

    Don't use gitignore with find files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,14 @@ return {
   --tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { "<leader>ff", function() builtin().find_files() end, desc = "Find [f]iles (telescope)" },
+    {
+      "<leader>ff",
+      function() builtin().find_files{
+        no_ignore = true,
+        no_ignore_parent = true,
+      } end,
+      desc = "Find [f]iles (telescope)"
+    },
     { "<C-p>",      function() builtin().git_files() end,  desc = "Find files in repo (telescope)" },
     { "<leader>fg", function() builtin().live_grep() end,  desc = "Live [g]rep (telescope)" },
     { "<leader>fb", function() builtin().buffers() end,    desc = "[b]uffers (telescope)" },
